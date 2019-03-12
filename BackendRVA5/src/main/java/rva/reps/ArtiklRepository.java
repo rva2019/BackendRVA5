0ef2dbe83ba55a062874dab338750c7371b693eb
markobojkic <marko.bojkic@uns.ac.rs>
2019-03-12 14:00:03 +0100

    Vezbe 3

---
+++ BackendRVA5/src/main/java/rva/reps/ArtiklRepository.java
@@ -0,0 +1,13 @@
+package rva.reps;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import rva.jpa.Artikl;
+
+public interface ArtiklRepository extends JpaRepository<Artikl, Integer> {
+	
+	Collection<Artikl> findByNazivContainingIgnoreCase(String naziv);
+
+}
